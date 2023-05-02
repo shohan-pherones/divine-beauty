272f887cba4f06ca6189229d8c119e9f21d469c3
ariful171042 <arif171042@gmail.com>
2023-05-02 23:05:52 +0600

    hero section responsive done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="hero-section relative h-[calc(100vh-5rem)] overflow-hidden bg-[#ece8e0]">
-      <div className="detals w-[850px] absolute  z-10 top-40 left-[550px]">
-        <h1 className="text-8xl font-[400]  leading-tight">
+      <div className="detals w-44 xl:w-[850px]  absolute left-[200px] top-28  md:top-40 xl:left-[580px] lg:w-[600px]  lg:left-[300px] md:w-[300px] md:left-72">
+        <h1 className=" text-xl  xl:text-8xl font-[400]  leading-tight  lg:text-6xl lg:leading-tight md:text-4xl md:leading-tight ">
           Grow up your skin with the power of natural product.
         </h1>
       </div>
 
       <div className="hero-wraper h-full container mx-auto grid grid-cols-2">
         <div className="left col-span-1 flex flex-col h-full justify-between">
-          <div className="img1 w-[300px] h-[400px] overflow-hidden">
+          <div className="img1 xl:w-[300px] xl:h-[400px] overflow-hidden lg:w-[250px] lg:h-[350px] md:w-[220px]">
             <Image
               priority
               src="https://res.cloudinary.com/dcvvwskjo/image/upload/v1682880388/divine-beauty/collection-natural-products_njrdws.jpg"
@@ -21,20 +21,20 @@ const Hero = () => {
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="img2 w-[300px] overflow-hidden">
+          <div className="img2 xl:w-[300px] lg:w-[250px] md:w-[220px]  overflow-hidden">
             <Image
               priority
               src="https://res.cloudinary.com/dcvvwskjo/image/upload/v1682897905/divine-beauty/young-woman-with-cosmetic-mask-her-face-makes-selfie-smartphone_duygrf.jpg"
               alt="hero image 2"
               width={500}
               height={500}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover hidden sm:block"
             />
           </div>
         </div>
 
         <div className="right col-span-1  flex justify-end items-end">
-          <div className="img3 w-[600px] h-[700px] end-0 overflow-hidden">
+          <div className="img3 xl:w-[600px] xl:h-[700px] lg:w-[350px] md:w-[330px] end-0 overflow-hidden">
             <Image
               priority
               src="https://res.cloudinary.com/dcvvwskjo/image/upload/v1682883884/divine-beauty/girl-with-face-mask_2_s9b8jj.jpg"
@@ -47,17 +47,19 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="btn1 absolute top-[510px] left-72 flex justify-center items-center cursor-pointer">
-        <div className="w-16 h-16 flex justify-center items-center bg-transparent absolute rounded-full border-2 border-r-transparent border-green-500/80 ">
-          <span className="h-3 w-3 rounded-full bg-green-500"></span>
+      <div className="hidden sm:block">
+        <div className="btn1 absolute top-60 left-[220px] xl:top-[450px] xl:left-72 sm:flex justify-center items-center cursor-pointer lg:top-[400px] lg:left-28 md:bottom-60 md:left-24 md:top-96">
+          <div className="w-8 h-8 sm:w-16 sm:h-16 flex justify-center items-center bg-transparent absolute rounded-full border-2 border-r-transparent border-green-500/80  ">
+            <span className="h-1 w-1 sm:h-3 sm:w-3 rounded-full bg-green-500 "></span>
+          </div>
+          <p className="absolute w-52 text-md sm:text-xl font-extralight left-4 z-10 ">
+            Browse our craft
+          </p>
         </div>
-        <p className="absolute w-52 text-xl font-extralight left-4 z-10 ">
-          Browse our craft
-        </p>
       </div>
 
-      <div className="btn1 absolute bottom-[100px] left-[800px]  cursor-pointer">
-        <p className=" w-48 py-3 px-2  text-center text-2xl font-extralight  bg-transparent border border-green-500/80  rounded-2xl shadow-lg hover:shadow-none">
+      <div className="btn1 absolute bottom-28 left-8 xl:bottom-[100px] xl:left-[800px]  cursor-pointer  lg:bottom-[50px] lg:left-[450px] md:left-[280px] md:bottom-16  ">
+        <p className=" w-32 text-md lg:w-48 py-3 px-2  text-center lg:text-2xl font-extralight  bg-transparent border border-green-500/80  rounded-2xl shadow-lg hover:shadow-none md:w-36 md:text-lg">
           Learn More ➟
         </p>
       </div>
